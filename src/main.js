750c78ea0d29eb0fb6737cb4e15e727fd5e2f988
pattodiablo <pattodiablo@gmail.com>
2022-05-13 09:47:23 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+
+function initGame(){
+var game = new Phaser.Game({
+		width: document.body.clientWidth,
+		height:  document.body.clientHeight,
+		type: Phaser.AUTO,
+        backgroundColor: "#242424",
+		scale: {
+			mode: Phaser.Scale.FIT,
+			autoCenter: Phaser.Scale.CENTER_BOTH
+		}
+	});
+	
+	game.scene.add("Preload", Preload);
+	game.scene.add("Level", Level);
+	game.scene.add("Boot", Boot, true);
+
+
+}
+	
+
+class Boot extends Phaser.Scene {
+
+	preload() {
+		
+		this.load.pack("pack", "assets/preload-asset-pack.json");
+
+		this.load.on(Phaser.Loader.Events.COMPLETE, () => this.scene.start("Preload"));
+	}
+}
---
+++ src/scenes/Level.js
@@ -0,0 +1,49 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+class Level extends Phaser.Scene {
+
+	constructor() {
+		super("Level");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @returns {void} */
+	editorCreate() {
+
+		// gameReady
+		const gameReady = this.add.text(400, 408, "", {});
+		gameReady.setOrigin(0.5, 0.5);
+		gameReady.text = "GAME IS READY";
+		gameReady.setStyle({ "fontFamily": "Arial", "fontSize": "30px" });
+
+		this.gameReady = gameReady;
+
+		this.events.emit("scene-awake");
+	}
+
+	/** @type {Phaser.GameObjects.Text} */
+	gameReady;
+
+	/* START-USER-CODE */
+
+	// Write more your code here
+
+	create() {
+
+		this.editorCreate();
+
+		this.gameReady.x=this.cameras.main.centerX;
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
